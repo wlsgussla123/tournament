7b152738d744081d08ced593f468cc7ba38493f3
Wooram Jun <chatii2412@gmail.com>
2017-12-05 23:52:32 +0900

    Implement dummy model

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,34 @@ import Tournament from '~/scripts/Tournament'
 
 const containerElement = document.querySelector('#container')
 
+const options = {
+	rounds: 5,
+
+	image: {
+		width: 200,
+		height: 300,
+	},
+}
+
+const getCandidates = (size) => {
+	const result = []
+
+	for (let i = 0; i < size; i += 1) {
+		result.push({
+			id: i,
+			image: {
+				url: `https://picsum.photos/${options.image.width}/${options.image.height}/?random`,
+				width: options.image.width,
+				height: options.image.height,
+			},
+		})
+	}
+
+	return result
+}
+
 const init = () => {
-	const candidates = [1, 2, 3, 4]
+	const candidates = getCandidates(2 ** options.rounds)
 
 	const tournament = new Tournament(containerElement, candidates)
 
